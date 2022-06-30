3dd23d9feba1cd754cfd3168e50adb10b9d9987a
rahul <609380+rahul27@users.noreply.github.com>
2022-06-30 09:53:34 -0700

    Release 1.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 buildscript {
     repositories {
@@ -13,6 +15,10 @@ buildscript {
     }
 }
 
+plugins {
+    id("com.github.ben-manes.versions").version(Versions.gradleVersionsPlugin)
+}
+
 allprojects {
     repositories {
         google()
@@ -26,4 +32,27 @@ allprojects {
 
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
+}
+
+fun isNonStable(version: String): Boolean {
+    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.toUpperCase().contains(it) }
+    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
+    val isStable = stableKeyword || regex.matches(version)
+    return isStable.not()
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    outputFormatter = closureOf<com.github.benmanes.gradle.versions.reporter.result.Result> {
+        if (outdated.dependencies.isEmpty()) {
+            println("Up to date!")
+        } else {
+            for (dependency in outdated.dependencies) {
+                println("${dependency.group} ${dependency.name} ${dependency.version} -> ${dependency.available.release ?: dependency.available.milestone}")
+            }
+        }
+    }
+    rejectVersionIf {
+        // ignore jacoco - https://github.com/ben-manes/gradle-versions-plugin/issues/534
+        isNonStable(candidate.version) || (candidate.group == "org.jacoco") && (candidate.version != currentVersion)
+    }
 }
