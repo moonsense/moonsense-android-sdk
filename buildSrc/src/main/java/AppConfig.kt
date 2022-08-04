ef55360426d5ade24552146614bb32d9eeb27b89
rahul <609380+rahul27@users.noreply.github.com>
2022-08-04 09:41:58 -0700

    set compileSdkVersion and targetSdkVersion to 32

--- buildSrc/src/main/java/AppConfig.kt
+++ buildSrc/src/main/java/AppConfig.kt
@@ -4,9 +4,9 @@
  */
 
 object AppConfig {
-    const val compileSdkVersion = 31
+    const val compileSdkVersion = 32
     const val minSdkVersion = 16
-    const val targetSdkVersion = 31
+    const val targetSdkVersion = 32
     const val versionCode = 1
     const val versionName = "1.1.0"
     const val buildToolsVersion = "30.0.3"
