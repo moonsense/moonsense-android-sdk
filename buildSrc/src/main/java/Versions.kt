9c37bd01cc4ce29dc8f2675876c8bae640048c03
moonsense-ci <87720312+moonsense-ci@users.noreply.github.com>
2022-10-27 14:50:00 -0400

    Release features-0.1.0 (#25)

--- buildSrc/src/main/java/Versions.kt
+++ buildSrc/src/main/java/Versions.kt
@@ -4,19 +4,19 @@
  */
 
 object Versions {
-    const val gradleTools = "7.3.0"
+    const val gradleTools = "7.3.1"
     const val kotlinVersion = "1.7.20"
     const val junit = "4.13.2"
     const val junitAndroidX = "1.1.3"
     const val espressoCore = "3.4.0"
     const val squareWireRuntime = "4.4.2"
     const val constraintLayout = "2.1.4"
-    const val fragmentKtx = "1.5.3"
+    const val fragmentKtx = "1.5.4"
     const val androidXLegacySupport = "1.0.0"
-    const val material = "1.6.1"
+    const val material = "1.7.0"
     const val retrofit = "2.9.0"
     const val gson = "2.9.1"
     const val mockWebServer = "4.10.0"
-    const val gradleVersionsPlugin = "0.42.0"
-    const val moonsenseFeatures = "0.1.0-alpha1"
+    const val gradleVersionsPlugin = "0.43.0"
+    const val moonsenseFeatures = "0.1.0"
 }
